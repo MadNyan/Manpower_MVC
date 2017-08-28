b1d694be6ef7ea32d2ae90c460c265f4069db16d
Unknown <ted15945@gmail.com>
2017-08-28 22:45:21 +0800

    User編輯列表 + 權限

--- Manpower_MVC/Controllers/UserController.cs
+++ Manpower_MVC/Controllers/UserController.cs
@@ -21,11 +21,13 @@ namespace Manpower_MVC.Controllers
         {
             User Get = (from p in db.User where p.Account == acc && p.Password == pwd select p).FirstOrDefault();
             Session["isLogin"] = null;
+            Session["isAdmin"] = null;
             Session["name"] = null;
             if (Get != null)
             {
                 Session["isLogin"] = Get.ID;
                 Session["name"] = Get.Name;
+                Session["isAdmin"] = Get.isAdmin.ToString();
                 return RedirectToAction("Index", "Home");
             }
             TempData["loginMsg"] = 0;
@@ -34,6 +36,7 @@ namespace Manpower_MVC.Controllers
         public ActionResult Logout()
         {
             Session["isLogin"] = null;
+            Session["isAdmin"] = null;
             Session["name"] = null;
             return RedirectToAction("Index", "Home");
         }
@@ -44,9 +47,53 @@ namespace Manpower_MVC.Controllers
         [HttpPost]
         public ActionResult Register(User user)
         {
+            user.isAdmin = 0;
             db.User.Add(user);
             db.SaveChanges();
             return RedirectToAction("Index", "Home");
+        }
+        public ActionResult Index()
+        {
+            if (Session["isAdmin"] != null)
+            {
+                if (Session["isAdmin"].Equals("1"))
+                {
+                    return View();
+                }
+            }
+            return RedirectToAction("Index", "Home");
+        }
+        public ActionResult getUser()
+        {
+            List<User> Get = (from p in db.User select p).ToList();
+            return Json(Get, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public ActionResult Create(User user)
+        {
+            db.User.Add(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(int id)
+        {
+            User Get = (from p in db.User where p.ID == id select p).FirstOrDefault();
+            return Json(Get, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public ActionResult Edit(User user)
+        {
+            User _user = (from p in db.User where p.ID == user.ID select p).FirstOrDefault();
+            _user.Name = user.Name;
+            _user.isAdmin = user.isAdmin;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int id)
+        {
+            db.User.Remove((from p in db.User where p.ID == id select p).FirstOrDefault());
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
